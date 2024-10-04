58c64adce1f6aa743ef8a5494b6af2306c9da1f2
HarshSaini3878 <142294106+HarshSaini3878@users.noreply.github.com>
2024-10-04 16:21:15 +0530

    script 2

--- 3rd/script.js
+++ 3rd/script.js
@@ -1,6 +1,25 @@
-var tl=gsap.timeline();
+
+var menu= document.querySelector("#nav i");
+var cros= document.querySelector("#full i");
+    var tl=gsap.timeline();
 tl.to("#full",{
     right:0,
     duration:0.8,
 
+})
+tl.from("#full h4",{
+   x:150,
+    duration:0.7,
+    stagger:0.25,
+    opacity:0,
+
+
+})
+tl.from("#full i",{
+    opacity:0,
+})
+
+tl.pause();
+menu.addEventListener("click",function(){
+    tl.play();
 })
